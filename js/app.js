f599a6f4b978cdfad24d3a8d4d48041e34072373
Your Name <you@example.com>
2022-05-10 22:00:43 +0300

    message

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 const menuBtn = document.querySelector('.posts__mobile__btn')
 const menuContent = document.querySelector('.posts__mobile__content')
-menuBtn.addEventListener('click',()=> {
+menuBtn?.addEventListener('click',function () {
     menuBtn.classList.toggle('active')
     menuContent.classList.toggle('active')
 })
+// swiper
+ const swiperJs = new Swiper ('.showcase-swiper', {
+    loop: true,
+     autoplay: {
+        delay: 4000,
+     },
+     speed: 700,
+     slidesPerView: 1.5,
+     spaceBetween: 16,
+     centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+     breakpoints: {
+         320: {
+             slidesPerView: 1,
+             spaceBetween: 16
+         },
+         1280: {
+             slidesPerView: 1.5,
+             spaceBetween: 16
+         }
+     }
+ });
